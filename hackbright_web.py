c37c958b572f57e22392c0a4203c030667c3e44c
Hackbright Student <no-reply@hackbrightacademy.com>
2017-04-25 18:03:17 -0700

    completed part 1

---
+++ hackbright_web.py
@@ -0,0 +1,57 @@
+"""A web application for tracking projects, students, and student grades."""
+
+from flask import Flask, request, render_template
+
+import hackbright
+
+app = Flask(__name__)
+
+
+@app.route("/student")
+def get_student():
+    """Show information about a student."""
+
+    github = request.args.get('github')
+
+    first, last, github = hackbright.get_student_by_github(github)
+    # import pdb; pdb.set_trace()
+    row = hackbright.get_grades_by_github(github)
+    html = render_template("student_info.html",
+                            first=first,
+                            last=last,
+                            github=github)
+
+    
+    return html
+
+@app.route("/student-search")
+def get_student_form():
+    """Show form for searching for a student."""
+
+    return render_template("student_search.html")
+
+
+
+@app.route("/new-student")
+def new_student():
+    """Creates new student"""
+    return render_template("new_student.html")
+
+@app.route("/new-student-database", methods=['POST'])
+def add_to_database():
+    """add info of new student to database"""
+    firstname = request.form.get('firstname')
+    lastname = request.form.get('lastname')
+    github = request.form.get('github')
+
+    hackbright.make_new_student(firstname, lastname, github)
+    return render_template("new_student_response.html",
+                            firstname=firstname,
+                            lastname=lastname,
+                            github=github)
+
+
+
+if __name__ == "__main__":
+    hackbright.connect_to_db(app)
+    app.run(debug=True)
